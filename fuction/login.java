da92262f31e5c749a904614f0624048ee96bed35
VitaminaNescau <vitorjss82@gmail.com>
2022-11-07 00:09:19 -0300

    classe q ira criar e fazer login

---
+++ fuction/login.java
@@ -0,0 +1,23 @@
+package fuction;
+//essa classe sera usada para login e cadastro de conta
+public class login {
+     user createUser = new user();
+    //cadastro
+    public void userSignUp(String name,String cpf,String email,String password,String cell){
+       
+        createUser.setNome(name);
+        createUser.setTelefone(cell);
+        createUser.setCPF(cpf);
+        createUser.setMail(email);
+        createUser.setPass(password);
+        
+        //corrigir valores de telefone no mysql
+        userDAO newUser = new userDAO();
+        newUser.signUP(createUser);
+    }
+    //login
+    public boolean userSignIn(){
+        
+        return true;
+    }
+}
