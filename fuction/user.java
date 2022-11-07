31f669d229cb8a47272f8822db3241ae84dde918
VitaminaNescau <vitorjss82@gmail.com>
2022-11-07 00:08:50 -0300

    classe contem  informaçoes necessarias do usuario

--- fuction/user.java
+++ fuction/user.java
@@ -1,10 +1,50 @@
 package fuction;
-//essa classe sera usada para login e cadastro de conta
-public class user {
-    public void userSignUp(String name,String cpf,String password){
 
+public class user {
+    private int id;
+    private String nome,telefone,mail,cpf,password;
+    private double saldo;
+
+    public int getId(){
+        return id;
     }
-    public boolean userSignIn(){
-        return true;
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone=telefone;
+    }
+    public String getMail(){
+        return mail;
+    }
+    public void setMail(String mail){
+        this.mail=mail;
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+    public void setSaldo(double saldo){
+        this.saldo=saldo;
+    }
+    public String getCPF(){
+        return cpf;
+    }
+    public void setCPF(String cpf){
+        this.cpf=cpf;
+    }
+    public String getPass(){
+        return password;
+    }
+    public void setPass(String password){
+        this.password=password;
     }
 }
