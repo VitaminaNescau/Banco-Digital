0bc2e318704e6f14cbfa7650ed437d04f2e5fed4
VitaminaNescau <vitorjss82@gmail.com>
2022-11-09 13:01:09 -0300

    modified:   body/teste.java

--- body/teste.java
+++ body/teste.java
@@ -1,16 +1,15 @@
 package body;
-import java.util.Calendar;
-import java.util.Locale;
-import java.util.Scanner;
-
-import fuction.*;
+import fuction.movi;
+import fuction.BD.login;
+import fuction.BD.user;
+import fuction.BD.userDAO;
 public class teste {
     public static void main(String[] args) {
       user t = new user();
       
       userDAO teste = new userDAO();
-        teste.signIN(t);
-
+       // teste.signIN(t);
+      movi mo = new movi();
       /*  t.setDate();;
       t.setNome("teste2");
       //t.setTelefone("71987908550");
@@ -22,11 +21,18 @@ public class teste {
       teste.signUP(t);*/
 
         login login = new login();
-        login.userSignUp("Davi", "10006264581", "@gmail.cxom", "davi123456", "71987908550");
+       /*  login.userSignUp("Davi", "10006264581", "@gmail.cxom", "davi123456", "71987908550");
         login.userSignUp("Bruno", "07466819508", "@gmail.cxom", "bruno123456", "71987908550");
         login.userSignUp("Douglas", "04016464595", "@gmail.cxom", "davi123456", "71987908550");
-
-     // login.userSignIn("07466819508","bruno123456");
+        */
+        teste.updateINFO(t);
+     //login.userSignIn("07466819508","bruno123456");
+     if (login.userSignIn("04016464595","davi123456")) {
+        System.out.println("sucesso");
+        mo.movita(t);
+     } else {
+        System.out.println("depressão");
+     }
 
 
 }
