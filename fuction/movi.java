e9970fcfc574f8e5396b4d847473e8b3fc86084c
VitaminaNescau <vitorjss82@gmail.com>
2022-11-10 21:21:23 -0300

    metodo do saque já está funcional

--- fuction/movi.java
+++ fuction/movi.java
@@ -1,19 +1,31 @@
 package fuction;
 
+import java.util.Scanner;
+
 import fuction.BD.user;
 import fuction.BD.userDAO;
 
-public class movi extends userDAO{
-    user teste = new user();
+public class movi {
+    user user = new user();
+    userDAO DAO = new userDAO();
     /*Permitir transferências (pix)
 Permitir depósitos para outros usuários. */
     public void  bcSaldo(user user){
-      user.setSaldo(-1500); 
+      user.setSaldo(10000.0); 
     }
-    public void movita(user user){
-        bcSaldo(user);
-        System.out.println("entour aq");
-        System.out.println(user.getCPF());
-        updateINFO(user);
+
+    /*metodo para se realizar saques, ele recebe o valor do saque 
+        e subtrai do saldo do banco de dados
+    */
+    public void saques(double value){
+        user.setSaque(value);
+            if (DAO.verifyUP(user, 2)) {
+                 user.setSaldo(user.getSaque()*(-1));
+                DAO.updateINFO(user);
+                System.out.println("Saque efetuado");
+            } else {
+                System.out.println("Saque não efetuado");
+            }
     }
+
 }
