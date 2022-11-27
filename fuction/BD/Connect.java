ca626db75b347c7d86910b81a91a6328f28aad95
VitaminaNescau <vitorjss82@gmail.com>
2022-11-27 14:14:02 -0300

    modified:   fuction/BD/Connect.java

--- fuction/BD/Connect.java
+++ fuction/BD/Connect.java
@@ -6,16 +6,17 @@ import java.sql.SQLException;
 
 
 public class Connect {
-    private final String USERNAME = "admin";
-    private final String PASSWORD = "123456789";
+    private final String USERNAME = "Vitor";
+    private final String PASSWORD = "brasil";
     //login e senha de acesso ao mysql
-    private final String URl_BC = "jdbc:mysql://banco-digital.czklbchcj5fm.us-east-1.rds.amazonaws.com/banco_digital";
+    private final String URl_BC = "jdbc:mysql://10.147.17.67/banco_digital";
     private Connection conect;
     //metodo q cria a conexão com o myslq
         public Connection conectMysql(){
         //try para captura um erro de conexão(o erro seria se o banco de dados não existisse)
             try {
             conect = DriverManager.getConnection(URl_BC, USERNAME, PASSWORD);
+            System.out.println("sucesso");
 
             } catch (SQLException e) {
                 System.out.println("SQLException: " + e.getMessage());
@@ -23,5 +24,10 @@ public class Connect {
                 System.out.println("VendorError: " + e.getErrorCode());
             }
             return conect;
+        }
+
+        public static void main(String[] args) {
+            Connect c = new Connect();
+            c.conectMysql();
         }
 }
